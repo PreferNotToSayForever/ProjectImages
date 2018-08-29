d30b45c9ca49c1d4427eaa25176ca68f8403d813
Quang Le Nguyen <lenguyenhaiquang9@gmail.com>
2018-08-29 11:12:02 +1000

    Initial Commit

---
+++ ProjectImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectImage
+//
+//  Created by Quang Le Nguyen on 29/8/18.
+//  Copyright © 2018 Quang Le Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
